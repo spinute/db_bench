11cd764c07842668b4658c8ff0b477578de4f73a
spinute <spinutids@gmail.com>
2015-05-20 20:46:35 +0900

    add rparsec samples and ipgen_build.txt

--- parsec_sample.rb
+++ parsec_sample.rb
@@ -1,1 +1,21 @@
 require 'rparsec'
+
+include RParsec
+class Calculator
+  include Parsers
+  include Functors
+  def parser
+    ops = OperatorTable.new.
+      infixl(char(?+) >> Plus, 20).
+      infixl(char(?-) >> Minus, 20).
+      infixl(char(?*) >> Mul, 40).
+      infixl(char(?/) >> Div, 40).
+      prefix(char(?-) >> Neg, 60)
+    expr = nil
+    term = integer.map(&To_i) | char('(') >> lazy{expr} << char (')')
+    delim = whitespace.many_
+    expr = delim >> Expressions.build(term, ops, delim)
+  end
+end
+ 
+Calculator.new.parser.parse '1+2*(3-1)' # => 5
